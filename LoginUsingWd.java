4e6796ad3adbe6c8fd3b1106c6f7e7a280b2e129
balajik212 <55569183+balajik212@users.noreply.github.com>
2020-04-12 23:56:25 +0530

    Week1.Day2.CW2

---
+++ LoginUsingWd.java
@@ -0,0 +1,23 @@
+package week1.day2;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class LoginUsingWd {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.setProperty("webdriver.chrome.driver", "chromedriver");
+		
+		ChromeDriver driver = new ChromeDriver();
+		
+		driver.get("https://accounts.google.com/ServiceLogin/signinchooser?service=mail&passive=true&rm=false&continue=https%3A%2F%2Fmail.google.com%2Fmail%2F&ss=1&scc=1&ltmpl=default&ltmplcache=2&emr=1&osid=1&flowName=GlifWebSignIn&flowEntry=ServiceLogin");
+		
+		driver.navigate().refresh();
+		String title = driver.getTitle();
+		System.out.println(title);
+		
+		driver.close();
+	}
+
+}
